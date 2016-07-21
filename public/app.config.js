10f8cbd7cb97c55e332594b4b73a573fb593fd6e
Loyce <lrlorenzo@gmail.com>
2016-07-21 17:45:43 +0800

    from simon

--- public/app.config.js
+++ public/app.config.js
@@ -36,7 +36,10 @@ angular.
 		when('/init', {
           template: '<project-login></project-login>'
         }).
-        otherwise('/init');
+		when('/view', {
+          template: '<document-view></document-view>'
+        }).
+        otherwise('/index');
     }
 	]);
 	
